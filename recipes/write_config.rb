0a08a840abead7ee1fb9394879738d940171b661
Doc Ritezel <pair+doc@ministryofvelocity.com>
2013-12-20 14:34:56 -0800

    Include restart_command so custom_env_template runs

--- recipes/write_config.rb
+++ recipes/write_config.rb
@@ -1,5 +1,6 @@
 # Set up app's custom configuration in the environment.
 # See https://forums.aws.amazon.com/thread.jspa?threadID=118107
+include_recipe "opsworks_custom_env::restart_command"
 
 node[:deploy].each do |application, deploy|
   
